19861c8c01d7b04c848d39bb479302eb039cbf6d
Lubaba Hasan <112156742+lubabahasan@users.noreply.github.com>
2025-01-07 22:36:59 +0600

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,11 +57,3 @@ function myFunction() {
   }
 }
 
-/*-------------- GLOWY CURSOR ---------------*/
-document.addEventListener("mousemove", (event) => {
-  let mousex = event.clientX - 15;
-  let mousey = event.clientY - 15;
-  let elem = document.querySelector('.cursor-glow');
-  elem.style.left = mousex + 'px';
-  elem.style.top = mousey + 'px';
-});
